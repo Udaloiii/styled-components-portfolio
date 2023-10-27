907ec1fcef982acf08131ecb9663de3a72a21616
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-27 15:17:08 +0300

    added style for Project.tsx; added images & animation line for tag a

--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -1,22 +1,31 @@
 import styled from "styled-components";
+import {FlexWrapper} from "src/components/FlexWrapper.tsx";
 
 type ProjectPropsType = {
     title: string
-    text: string
-    link?: string
+    text?: string
+    linkDemo?: string
+    linkCode?: string
     logo?: string
 }
-export const Project = ({title, text, logo, link}: ProjectPropsType) => {
+export const Project = ({title, text, logo, linkDemo, linkCode}: ProjectPropsType) => {
     return (
-        <StyledLink href={link} target={"_blank"}>
-            <StyledProject>
-                <StyledTitle>
-                    {title}
-                </StyledTitle>
-                <StyledImg src={logo} alt="logo"/>
+        <StyledProject>
+            <StyledImg src={logo} alt="logo"/>
+            <StyledTitle>
+                {title}
+            </StyledTitle>
+            <FlexWrapper>
+                <StyledLink href={linkDemo} target={"_blank"}>
+                    Demo
+                </StyledLink>
+                <StyledLink href={linkCode} target={"_blank"}>
+                    Code
+                </StyledLink>
+            </FlexWrapper>
 
-            </StyledProject>
-        </StyledLink>
+        </StyledProject>
+
 
     )
 }
@@ -34,19 +43,59 @@ const StyledProject = styled.div`
   }
 `
 
-const StyledLink = styled.a`
-  width: 100%;
-  height: 100%;
-  color: #242424;
-`
-
 const StyledTitle = styled.h3`
   display: flex;
   justify-content: center;
+  color: #242424;
+  font-family: Josefin Sans, sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 1px;
+  padding: 10px;
 `
 
 const StyledImg = styled.img`
-  width: 90%;
-  height: 90%;
+  width: 400px;
+  height: 250px;
   object-fit: cover;
+  border-top-right-radius: 14px;
+  border-top-left-radius: 14px;
+`
+const StyledLink = styled.a`
+  position: relative;
+  font-family: Poppins, sans-serif;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: black;
+  padding: 20px;
+
+  //&:hover {
+  //  text-decoration: underline;
+  //  text-decoration-color: cornflowerblue;
+  //  text-decoration-thickness: 5px;
+  //}
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 12px;
+    bottom: 15px;
+    width: 70%;
+    height: 4px;
+    background-color: cornflowerblue;
+    visibility: hidden;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  &:hover::after {
+    visibility: visible;
+    transform: scaleX(1);
+  }
 `
