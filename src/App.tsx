5e728f4c40ded9f71c2d44eaea0b6253d1da3503
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-29 16:56:04 +0300

    created Footer.tsx and added his in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,20 @@ import {Main} from "src/layout/sections/main/Main.tsx";
 import {Skills} from "src/layout/sections/skills/Skills.tsx";
 import {Works} from "src/layout/sections/projects/Works.tsx";
 import {Contacts} from "src/layout/sections/contacts/Contacts.tsx";
+import {Footer} from "src/layout/footer/Footer.tsx";
 
 
 function App() {
     return (
         <div className="App">
-            {/*<Title>Welcome to IT-INCUBATOR</Title>*/}
             <Header/>
             <Main/>
             <Skills/>
             <Works/>
             <Contacts/>
+            <Footer/>
         </div>
     );
 }
 
 export default App;
-
-// const Title = styled.h1`
-//   font-size: 1.5em;
-//   text-align: center;
-//   color: #e91e63;
-// `;
