48b5558b0209d19d1fead196b04b4f00af78bf45
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-31 19:32:12 +0300

    added google-fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import {GlobalStyle} from "src/styles/Global.styled.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter><App/></BrowserRouter>
+    <BrowserRouter>
+        <GlobalStyle/>
+        <App/>
+    </BrowserRouter>
 )
