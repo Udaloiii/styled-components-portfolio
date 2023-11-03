906263285dc90c967eedeb3302ef55537e813b2a
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-03 13:16:36 +0300

    added line-height: 1.2 in global styles

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -16,6 +16,7 @@ export const GlobalStyle = createGlobalStyle`
     //background: url("https://img.freepik.com/free-vector/geometric-pattern-with-lines-dots-3d-effect_1319-93.jpg?w=1380&t=st=1698759173~exp=1698759773~hmac=09957926479d615ee0c0f8856f47a0c4e4bfa8a56651a0ed652e6926b5a5e8ec") center repeat !important;
     background: url("https://img.freepik.com/free-vector/hand-drawn-doodle-icons-set_1308-90706.jpg?w=1380&t=st=1698763328~exp=1698763928~hmac=50bb66bc54ce3c8a2718a8ef194f981166e837fa2804e6f3ab42f32e426bb713") center repeat !important;
     color: ${theme.color.font};
+    line-height: 1.2;
   }
 
   a {
