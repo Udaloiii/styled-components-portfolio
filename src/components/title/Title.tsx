dacfc3dbb3660392d9b85326cdf166349f02e8bd
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-04 13:15:46 +0300

    added pseudo-element (under title) and added margin for title

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,11 +1,27 @@
 import styled from "styled-components";
+import {theme} from "src/styles/Theme.tsx";
 
 
 export const Title = styled.h2`
+  position: relative;
   font-family: "Broadway", sans-serif;
+  text-shadow: 1px 1px 2px ${theme.color.accent};
   font-size: 1.5rem;
   letter-spacing: 3px;
   display: flex;
   justify-content: center;
   padding: 20px 0;
+  margin-bottom: 90px;
+  
+  &::before {
+    content: "";
+    position: absolute;
+    display: inline-block;
+    width: 55px;
+    height: 1px;
+    background-color: ${theme.color.accent};
+    left: 50%;
+    bottom:-10px;
+    transform: translateX(-50%);
+  }
 `
