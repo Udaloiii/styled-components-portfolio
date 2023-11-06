52efd652836985b3acaae9c127f3848d0aae5119
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-06 15:40:28 +0300

    added media-requests for Theme.tsx

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,8 +1,12 @@
 export const theme = {
     color: {
-        // primary: "",
-        // secondary: "",
+        primary: "#4A4A4A",
+        secondary: "#252527",
         accent: "#7572D5",
         font: "#FFFFFF",
+    },
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)"
     }
 }
