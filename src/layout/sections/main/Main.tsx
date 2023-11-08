55c452a9086ef7872fab0d73a2dea958ec6d8113
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-08 19:02:53 +0300

    added mixin for Main.tsx; changed style

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,17 +3,18 @@ import avatar from '../../../assets/image/avatar1.webp'
 import styled from "styled-components";
 import {Container} from "src/components/container/Container.tsx";
 import {theme} from "src/styles/Theme.tsx";
+import {font} from "src/styles/Common.ts";
 
 export const Main = () => {
     return (
         <StyleMain id="Home">
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
-                    <FlexWrapper direction={"column"} justify={"center"} gap={"20px"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
+                    <div>
                         <Text>Hi there!</Text>
-                        <Name>I'm <span> Eugene Udalykh</span></Name>
-                        <MainTitle>Im frontend developer</MainTitle>
-                    </FlexWrapper>
+                        <Name>I am <span> Eugene Udalykh</span></Name>
+                        <MainTitle>I am frontend developer</MainTitle>
+                    </div>
                     <WrapperForImg><StyledImg src={avatar} alt="Eugene Udalykh"/></WrapperForImg>
                 </FlexWrapper>
             </Container>
@@ -24,61 +25,62 @@ const StyleMain = styled.section`
   display: flex;
   width: 100%;
   min-height: 100vh;
-  //height: 100vh; // добавил чтобы длина была на всю страницу если уберу, то контейнер будет по длине картинки)
   padding-top: 60px; // чтобы при скролле учитывало длину Header
-  //background-color: #02323f;
 `
 
 const WrapperForImg = styled.div`
   position: relative;
   z-index: 0;
-  
+
   &::before {
     content: "";
     position: absolute;
     display: inline-block;
     top: -24px;
-    right: -24px;
+    right: -34px;
     width: 360px;
-    height: 495px;
+    height: 470px;
     border: 5px solid ${theme.color.accent};
     z-index: -1;
+
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -20px;
+      right: -21px;
+    }
   }
 
 `
 const StyledImg = styled.img`
-  //border: 1px solid #e5e4e4;
   background: rgba(215, 215, 215, 0.95);
   box-shadow: 0 0 8px 5px rgba(232, 232, 232, 0.8);
   border-radius: 10px;
   width: 350px;
-  height: 450px;
+  height: 430px;
   object-fit: cover;
+
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `
 
 const MainTitle = styled.h1`
+  ${font({Fmin: 20, Fmax: 27, weight: 400})}
   display: flex;
-  color: #FFF;
-  font-size: 27px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-
 `
 const Name = styled.h2`
+  ${font({family: "'Josefin Sans', sans-serif", Fmax: 50, Fmin: 36, weight: 700})};
+  margin: 20px 0;
+  letter-spacing: 0.05rem;
   display: flex;
-  color: #FFF;
-  font-family: Josefin Sans, sans-serif;
-  font-size: 50px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  letter-spacing: 2.5px;
+  flex-wrap: wrap;
 
   span {
     position: relative;
-
-    z-index: 0;  // для того, чтобы полоска была "под" текстом
+    z-index: 0; // для того, чтобы полоска была "под" текстом
+    white-space: nowrap;
 
     &::before {
       content: "";
@@ -88,7 +90,7 @@ const Name = styled.h2`
       width: 100%;
       background-color: ${theme.color.accent};
       height: 20px;
-      z-index: -1;  // для того, чтобы полоска была "под" текстом
+      z-index: -1; // для того, чтобы полоска была "под" текстом
     }
   }
 `
