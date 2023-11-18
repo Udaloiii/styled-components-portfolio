ede1d6ce7543fdd0fd10be612891d44c51c3fe1a
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-18 18:58:41 +0300

    added images in works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -6,6 +6,9 @@ import {Container} from "src/components/container/Container.tsx";
 import {FC, useState} from "react";
 import {S} from './Works_Styles.ts'
 import {AnimatePresence, motion} from "framer-motion";
+import todos from '../../../assets/image/todos.webp'
+import network from '../../../assets/image/soc-network.webp'
+import cards from '../../../assets/image/cards.webp'
 
 type ProjectType = {
     id: number
@@ -24,21 +27,21 @@ const arrOfProjects: ProjectType[] = [
         text: "",
         linkDemo: "https://todolist-on-redux.vercel.app/",
         linkCode: "https://github.com/Udaloiii/todolist_on_redux",
-        logo: "https://img.freepik.com/free-vector/lovely-hand-drawn-list-collection_23-2147934745.jpg?w=1480&t=st=1698342879~exp=1698343479~hmac=6b5342ce997d64d383611e044c28ca4d0c59162da8ba1fb5ec9e95763cdbbe51",
+        logo: todos,
         type: "spa"
     },
     {
         id: 2,
         title: "Social network",
         text: "",
-        logo: "https://img.freepik.com/free-vector/social-media-network_74855-4575.jpg?w=2000&t=st=1698406524~exp=1698407124~hmac=668a0b59b0bde71b2d165f884fa85f7f043c0edb572f4d777a40d334ba2d1839",
+        logo: network,
         type: "spa"
     },
     {
         id: 3,
         title: "Cards",
         text: "",
-        logo: "https://img.freepik.com/free-vector/hand-drawn-months-year-element-collection_23-2149923253.jpg?w=1480&t=st=1698406908~exp=1698407508~hmac=594e1cdde88851b6230d82ae5ccbe4db1763cca1544cea5a3161104ae3c22119",
+        logo: cards,
         type: "react"
     },
 
