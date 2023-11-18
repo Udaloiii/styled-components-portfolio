3e9e72ecd8f18cda039a394f26e79d89573595a6
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-18 19:00:32 +0300

    added background-img in Global.styled.ts

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -1,5 +1,6 @@
 import {createGlobalStyle} from "styled-components";
 import {theme} from "src/styles/Theme.tsx";
+import background from '../assets/image/background.jpg'
 
 export const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
@@ -13,8 +14,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Poppins', Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    //background: url("https://img.freepik.com/free-vector/geometric-pattern-with-lines-dots-3d-effect_1319-93.jpg?w=1380&t=st=1698759173~exp=1698759773~hmac=09957926479d615ee0c0f8856f47a0c4e4bfa8a56651a0ed652e6926b5a5e8ec") center repeat !important;
-    background: url("https://img.freepik.com/free-vector/hand-drawn-doodle-icons-set_1308-90706.jpg?w=1380&t=st=1698763328~exp=1698763928~hmac=50bb66bc54ce3c8a2718a8ef194f981166e837fa2804e6f3ab42f32e426bb713") center repeat !important;
+    background: url(${background}) 0 0  / cover fixed !important;
     color: ${theme.color.font};
     line-height: 1.2;
     min-width: 360px;
